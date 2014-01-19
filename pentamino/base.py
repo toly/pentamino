6a3da99e94ea4590b0545b96543ea3960ccdd6a7
toly <tolytrader@gmail.com>
2014-01-19 13:42:13 +0400

    check figure init data

--- pentamino/base.py
+++ pentamino/base.py
@@ -1,5 +1,7 @@
 __author__ = 'toly'
 
+import itertools
+
 
 class BaseObject(object):
     """
@@ -28,3 +30,40 @@ class Board(BaseObject):
         row = [0] * width
         for i in xrange(heigh):
             self.data.append(list(row))
+
+
+class Figure(BaseObject):
+    data = None
+    width = None
+    height = None
+
+    shift = None
+    hash_key = None
+
+    def __init__(self, data):
+
+        self.data = data
+        self.check_size()
+        self.check_single_color()
+
+        self.shift = self.data[0].index(1)
+
+    def check_size(self):
+        width = None
+        for i in xrange(len(self.data)):
+            next_width = len(self.data[i])
+            if next_width == 0:
+                raise Exception('Empty row')
+            if not width is None and width != next_width:
+                raise Exception('Different lengths of rows')
+            width = next_width
+
+    def check_single_color(self):
+        all_colors = set(itertools.chain(**self.data))
+        not_null_colors = list(all_colors - {0})
+        if len(not_null_colors) == 0:
+            raise Exception('Empty figure')
+        if len(not_null_colors) > 1:
+            raise Exception('Too many colors')
+        if not_null_colors[0] != 1:
+            raise Exception('Need only one color: 1')
